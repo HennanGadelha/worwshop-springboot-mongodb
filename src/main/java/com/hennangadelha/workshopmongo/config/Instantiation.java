71ca6ea2325f3b4c09b381d590a7d9075abf81d5
Hennan Gadelha <hennanhennan@gmail.com>
2020-06-28 18:02:11 -0300

    instanciaçao da base de dados

---
+++ src/main/java/com/hennangadelha/workshopmongo/config/Instantiation.java
@@ -0,0 +1,38 @@
+package com.hennangadelha.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.hennangadelha.workshopmongo.domain.User;
+import com.hennangadelha.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	
+	
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+	}
+
+	
+	
+	
+}
